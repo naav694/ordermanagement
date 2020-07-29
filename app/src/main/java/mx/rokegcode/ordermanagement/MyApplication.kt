e5201638364a956350c6ea600f41443ac1cb32df
naav694 <e.ivangarcia94@gmail.com>
2020-07-28 23:59:44 -0500

    Changes: * Added a new parameter for Customer.kt, it was lastName

--- app/src/main/java/mx/rokegcode/ordermanagement/MyApplication.kt
+++ app/src/main/java/mx/rokegcode/ordermanagement/MyApplication.kt
@@ -42,7 +42,6 @@ class MyApplication : Application() {
                 helperModule
             )
         }
-
         registerBroadcast()
     }
 
--- app/src/main/java/mx/rokegcode/ordermanagement/model/data/User.kt
+++ app/src/main/java/mx/rokegcode/ordermanagement/model/data/User.kt
@@ -1,16 +1,13 @@
 package mx.rokegcode.ordermanagement.model.data
 
-import android.os.Parcelable
 import androidx.room.Entity
 import androidx.room.PrimaryKey
-import kotlinx.android.parcel.Parcelize
 
 @Entity(tableName = "user")
-@Parcelize
 data class User(
     var fullName: String = "",
     var userName: String = "",
     var userPassword: String = "",
     @PrimaryKey(autoGenerate = true)
     var idUser: Long = 0
-) : Parcelable
+)
