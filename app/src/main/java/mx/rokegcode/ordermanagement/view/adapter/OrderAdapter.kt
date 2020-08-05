30f9ca6dcd5b8d768578990ef42ac10231a03d33
naav694 <e.ivangarcia94@gmail.com>
2020-08-05 00:19:45 -0500

    Changes: * Changed the item view and Added the fields to the OrderAdapter.kt

--- app/src/main/java/mx/rokegcode/ordermanagement/view/adapter/OrderAdapter.kt
+++ app/src/main/java/mx/rokegcode/ordermanagement/view/adapter/OrderAdapter.kt
@@ -3,9 +3,14 @@ package mx.rokegcode.ordermanagement.view.adapter
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.button.MaterialButton
+import kotlinx.android.synthetic.main.item_order.view.*
 import mx.rokegcode.ordermanagement.R
 import mx.rokegcode.ordermanagement.model.data.Order
+import java.text.NumberFormat
+import java.util.*
 
 class OrderAdapter(private var orderList: List<Order>) :
     RecyclerView.Adapter<OrderAdapter.OrderViewHolder>() {
@@ -20,9 +25,24 @@ class OrderAdapter(private var orderList: List<Order>) :
 
     override fun onBindViewHolder(holder: OrderViewHolder, position: Int) {
         val order = orderList[position]
+        holder.textDeliveryDate.text = order.deliveryDate
+        holder.textProductDesc.text = order.productDescription
+        holder.textPrice.text =
+            NumberFormat.getCurrencyInstance(Locale.getDefault()).format(order.productPrice)
+        holder.btnFinishOrder.setOnClickListener {
+
+        }
+        holder.btnViewOrderDetail.setOnClickListener {
+
+        }
     }
 
     class OrderViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-
+        val textDeliveryDate: TextView = view.textDeliveryDate
+        val textClientOrder: TextView = view.textClientOrder
+        val textProductDesc: TextView = view.textProductDesc
+        val textPrice: TextView = view.textPrice
+        val btnViewOrderDetail: MaterialButton = view.btnViewOrderDetail
+        val btnFinishOrder: MaterialButton = view.btnFinishOrder
     }
 }
