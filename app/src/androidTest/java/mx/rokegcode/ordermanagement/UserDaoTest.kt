4c186a40d38c557348f83ed4e121da443df40379
Edgar Iván Rodríguez García <46581013+naav694@users.noreply.github.com>
2020-09-23 15:50:06 -0400

    Commit For Testing

--- app/src/androidTest/java/mx/rokegcode/ordermanagement/UserDaoTest.kt
+++ app/src/androidTest/java/mx/rokegcode/ordermanagement/UserDaoTest.kt
@@ -3,9 +3,7 @@ package mx.rokegcode.ordermanagement
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import mx.rokegcode.ordermanagement.model.db.AppDatabase
 import mx.rokegcode.ordermanagement.model.db.UserDao
-import org.junit.Before
 import org.junit.runner.RunWith
-import org.koin.core.context.startKoin
 import org.koin.test.KoinTest
 import org.koin.test.inject
 
